a197853d121796a59ec79e77acf2914191d499d4
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-02-18 17:02:15 +0100

    fddfd

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import Typed from 'typed.js';
+
 
 Rails.start()
 Turbolinks.start()
