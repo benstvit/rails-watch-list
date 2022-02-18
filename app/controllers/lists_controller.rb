97383e31b93a9219e724b1d514b75b8628a4dd29
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-02-18 15:26:54 +0100

    photo

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class ListsController < ApplicationController
   before_action :find_list, only: %i[show]
 
@@ -14,13 +16,21 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(params_list)
+    if params_list[:photo].nil?
+      file = URI.open("https://picsum.photos/seed/picsum/200/300")
+      @list = List.new(name: params_list[:name])
+      @list.photo.attach(io: file, filename: 'placeholder.jpg', content_type: 'image/jpg')
+    else
+      @list = List.new(params_list)
+    end
     if @list.save
       redirect_to list_path(@list), notice: 'Restaurant created!'
     else
       render :new
     end
   end
+
+  # Dans params de list create if photo = nil then attach url
 
   private
 
@@ -29,6 +39,6 @@ class ListsController < ApplicationController
   end
 
   def params_list
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
